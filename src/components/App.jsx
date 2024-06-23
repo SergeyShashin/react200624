8a8408be876b0a4e439b16005b0e761832004390
SergeyShashin <s9997910@yandex.ru>
2024-06-23 16:34:52 +0300

    React 200624 lesson2 handleClick done.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-export class App extends Component{
-  
-  
-  render(){
-    return(
-      <div>Привет из App.</div>
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+
+  }
+
+  handleClick = (e) => {
+    this.setState(prevState => prevState.counter += Number(e.target.dataset.counter));
+  }
+
+  render() {
+    let { counter } = this.state;
+    return (
+      <div>
+        <h3>Привет из App</h3>
+        <button data-counter={+1} onClick={this.handleClick}>+</button>
+        <span>{counter}</span>
+        <button data-counter={-1} onClick={this.handleClick}>-</button>
+      </div>
     );
   }
 }
